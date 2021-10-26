7df7d2fc415ce4c3f454279d7212513c399f5cfe
OlegKotov <olegkotov003@gmail.com>
2021-10-26 23:34:29 +0300

    start project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+      <nav className="nav">
+        <div>
+          <a href="n">Profile</a>
+        </div>
+        <div>
+          <a href="n">Messeges</a>
+        </div>
+        <div>
+          <a href="n">News</a>
+        </div>
+        <div>
+          <a href="n">Music</a>
+        </div>
+        <div>
+          <a href="n">Setting</a>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1glsxlx0_XtsuRrlbu1RnDH0oFSLh7IJC0Q&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlrZqTCInyg6RfYC7Ape20o-EWP1EN_A8fOA&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div>
+          My post
+          <div>New post</div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
